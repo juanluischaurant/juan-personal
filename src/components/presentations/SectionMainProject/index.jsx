e7acd19ad3f91e24358cf9006fbb83fe4593e071
Juan Luis Chaurant <juanluischaurant@gmail.com>
2022-03-01 15:55:26 -0400

    updated image component

--- src/components/presentations/SectionMainProject/index.jsx
+++ src/components/presentations/SectionMainProject/index.jsx
@@ -7,22 +7,22 @@ import { ThemeProvider } from 'styled-components'
 
 const OuterContainer = styled.section`
   padding-inline: 1em;
-	background: ${props=>props.theme.colors.primaryColor};
-	
-	display: flex;
+  background: ${(props) => props.theme.colors.primaryColor};
+
+  display: flex;
   justify-content: center;
   align-items: center;
 `
 
 const InnerContainer = styled.div`
   margin: 2em auto 4em auto;
-	width: 95%;
+  width: 95%;
   max-width: 1000px;
-	
-	@media ((min-width: ${props=>props.theme.breakpoints.tabletX1})) {
+
+  @media ((min-width: ${(props) => props.theme.breakpoints.tabletX1})) {
     width: 80%;
   }
-  @media ((min-width: ${props=>props.theme.breakpoints.extraLargeX1})) {
+  @media ((min-width: ${(props) => props.theme.breakpoints.extraLargeX1})) {
     width: 70%;
   }
 `
@@ -34,10 +34,10 @@ const HeaderContainer = styled.div`
   margin: 0 0 1.4em;
 `
 const SectionTitle = styled.h2`
-  color: ${({theme}) => theme.colors.fontMainColor};
+  color: ${({ theme }) => theme.colors.fontMainColor};
   margin: 0;
   padding: 0;
-  font-size: ${props=>props.theme.fontScale.smallScreen.medium};
+  font-size: ${(props) => props.theme.fontScale.smallScreen.medium};
 `
 
 const Line = styled.div`
@@ -60,10 +60,10 @@ const BodyContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  
+
   margin-top: calc(4em + 2em);
-  
-  @media ((min-width: ${props=>props.theme.breakpoints.largeX1}) ) {
+
+  @media ((min-width: ${(props) => props.theme.breakpoints.largeX1})) {
     flex-direction: row;
     column-gap: 4em;
   }
@@ -84,16 +84,14 @@ const FeaturedContainer = styled.div`
 `
 
 const FeaturedContainerImage = styled.div`
-  width: 80%;
-  height: 80%;
+  width: 90%;
+  /* height: 80%; */
   max-width: 30em;
   max-height: 30em;
   transform: translateY(-4em);
-
 `
-const Image = styled.img.attrs({
-  src: 'https://assets-global.website-files.com/5fda3048302e579473bfb454/6081869d03bc45778e9e752c_CleanShot%20Slite%20Project%20Proposal%20Template%20-%20Project%20Proposal%20Template%20at%2018.21.39.png'
-})`
+
+const Image = styled.img.attrs((props) => ({ src: props.imageUrl }))`
   max-width: 100%;
 `
 
@@ -101,20 +99,19 @@ const FeaturedContainerContent = styled.div`
   max-width: 85%;
 `
 
-
 const FeaturedContainerDate = styled.div`
-  color: ${({theme}) => theme.colors.accentColor};
+  color: ${({ theme }) => theme.colors.accentColor};
 `
 const FeaturedContainerTitle = styled.h3`
-  color: ${({theme}) => theme.colors.fontMainColor};
+  color: ${({ theme }) => theme.colors.fontMainColor};
 
   font-size: var(--fs-500);
   margin-top: 0;
-  margin-bottom: .0em;
+  margin-bottom: 0em;
 `
 
 const FeaturedContainerText = styled.div`
-  color: ${({theme}) => theme.colors.fontBodyColor};
+  color: ${({ theme }) => theme.colors.fontBodyColor};
 
   line-height: 1.5em;
   padding-bottom: 1em;
@@ -122,7 +119,7 @@ const FeaturedContainerText = styled.div`
 
 const FeaturedContainerFooter = styled.div`
   & > *:first-child {
-    margin-bottom: .8em;
+    margin-bottom: 0.8em;
   }
 `
 
@@ -135,23 +132,21 @@ const LinkContainer = styled.div`
 `
 
 const LinkItem = styled.div`
-    font-size: ${props=>props.theme.fontScale.smallScreen.small};
-    margin-right: 8px;
-    color: rgb(95, 126, 151);
+  font-size: ${(props) => props.theme.fontScale.smallScreen.small};
+  margin-right: 8px;
+  color: rgb(95, 126, 151);
 `
 const IconContainer = styled.div`
   display: flex;
   justify-content: flex-end;
-  column-gap: .4em;
+  column-gap: 0.4em;
   /* align-items: flex-end; */
-  color: ${props=>props.theme.colors.fontMainColor};
+  color: ${(props) => props.theme.colors.fontMainColor};
   margin-top: 8px;
 
   & > * {
     cursor: pointer;
   }
-  
-
 `
 
 const SectionMainProject = () => {
@@ -159,30 +154,39 @@ const SectionMainProject = () => {
     <ThemeProvider theme={theme}>
       <OuterContainer>
         <InnerContainer>
-          
           <HeaderContainer>
             <LeftLine />
             <SectionTitle>Les Projects</SectionTitle>
             <Line />
           </HeaderContainer>
-          
+
           <BodyContainer>
-
             <FeaturedContainer>
-              
               <FeaturedContainerImage>
-                <Image></Image>
+                <Image
+                  imageUrl={
+                    'http://res.cloudinary.com/dm7mqsupu/image/upload/v1646162180/react-calculator_z2sgg0.png'
+                  }
+                />
               </FeaturedContainerImage>
-              
+
               <FeaturedContainerContent>
-                
-                <FeaturedContainerDate>October 25 - November 1, 2021</FeaturedContainerDate>
+                <FeaturedContainerDate>
+                  October 25 - November 1, 2021
+                </FeaturedContainerDate>
                 <FeaturedContainerTitle>A cool Project</FeaturedContainerTitle>
-                
+
                 <FeaturedContainerText>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, unde? Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, est!</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, unde?</p>
-                </FeaturedContainerText>  
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+                    unde? Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Temporibus, est!
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+                    unde?
+                  </p>
+                </FeaturedContainerText>
 
                 <FeaturedContainerFooter>
                   <LinkContainer>
@@ -196,27 +200,36 @@ const SectionMainProject = () => {
                     <FiGithub size="2rem"></FiGithub>
                   </IconContainer>
                 </FeaturedContainerFooter>
-
               </FeaturedContainerContent>
-
             </FeaturedContainer>
             {/* end of FeaturedContainer */}
 
             <FeaturedContainer>
-              
               <FeaturedContainerImage>
-                <Image></Image>
+                <Image
+                  imageUrl={
+                    'http://res.cloudinary.com/dm7mqsupu/image/upload/v1646162180/react-calculator_z2sgg0.png'
+                  }
+                />
               </FeaturedContainerImage>
-              
+
               <FeaturedContainerContent>
-                
-                <FeaturedContainerDate>October 25 - November 1, 2021</FeaturedContainerDate>
+                <FeaturedContainerDate>
+                  October 25 - November 1, 2021
+                </FeaturedContainerDate>
                 <FeaturedContainerTitle>A cool Project</FeaturedContainerTitle>
-                
+
                 <FeaturedContainerText>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, unde? Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, est!</p>
-                  <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A, unde?</p>
-                </FeaturedContainerText>  
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+                    unde? Lorem ipsum dolor sit amet consectetur adipisicing
+                    elit. Temporibus, est!
+                  </p>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. A,
+                    unde?
+                  </p>
+                </FeaturedContainerText>
 
                 <FeaturedContainerFooter>
                   <LinkContainer>
@@ -230,23 +243,14 @@ const SectionMainProject = () => {
                     <FiGithub size="2rem"></FiGithub>
                   </IconContainer>
                 </FeaturedContainerFooter>
-
               </FeaturedContainerContent>
-
             </FeaturedContainer>
             {/* end of FeaturedContainer */}
-
-            
-
           </BodyContainer>
         </InnerContainer>
       </OuterContainer>
-
     </ThemeProvider>
   )
 }
 
 export default SectionMainProject
-
-    
-
